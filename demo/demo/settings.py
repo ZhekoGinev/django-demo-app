798bb6f0b445ba0dcfa99bef330ffd6ed89f5b5f
zhekoginev <jginev@gmail.com>
2022-12-05 11:11:59 +0200

    add allowed hosts

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-^s2!^a&4e09-o&*5b*!&8t5ncy%$&b*ai@h50@=xm0jzvxe98#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    0.0.0.0,
+    127.0.0.1,
+    localhost,
+]
 
 
 # Application definition
